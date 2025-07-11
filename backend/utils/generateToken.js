9824f343c93887e6c0b4546f3f96e782242f89b9
prayash <prayashsingh608@gmail.com>
2025-07-11 19:40:38 +0530

    noting

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,8 @@ import jwt from "jsonwebtoken"
         secure:false,
         sameSite:'strict',  
         // maxAge:15*24*60*60*1000,
-        expires: new Date(Date.now() + 20 * 24 * 60 * 60 * 1000)
+        expires: new Date(Date.now() + 20 * 24 * 60 * 60 * 1000),
+        secure:process.env.NODE_ENV !== "developement",
     })
   }
 
